1322425f59f2b21be8332919308ceab85bd9c57b
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-08-29 16:16:17 +1000

    Defined a file saver interface.

---
+++ backup/files.go
@@ -0,0 +1,29 @@
+// Copyright 2012 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package backup
+
+import (
+	"github.com/jacobsa/comeback/blob"
+	"io"
+)
+
+// An object that knows how to save files to some underlying storage.
+type FileSaver interface {
+	// Save the contents of the supplied reader to underlying storage, returning
+	// a list of scores of blobs that should be concatenated in order to recover
+	// its contents.
+	Save(r io.Reader) (scores []blob.Score, err error)
+}
