801e2043f28a02ed8aad29434a1ae977b8aec6ec
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-08-29 21:23:44 +1000

    Moved package comments.

--- blob/blob.go
+++ blob/blob.go
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package blob contains types related to storage of content-addressed blobs.
 package blob
 
 import (
