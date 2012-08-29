4c26f04880726b24f1a05548a9c55a5cab7b48bc
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-08-29 14:49:02 +1000

    Defined a Score type.

---
+++ blob/blob.go
@@ -0,0 +1,28 @@
+// Copyright 2012 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package blob contains types related to storage of content-addressed blobs.
+package blob
+
+// A Score is the identifier for a blob previously stored by a blob store. It
+// consists of a hash of the blob's contents, so that with high probability two
+// blobs have the same contents if and only if they have the same score.
+type Score interface {
+	// Return the 20-byte 'raw' SHA-1 hash of the blob's contents.
+	Sha1Hash() string
+
+	// Return a 40-digit hex SHA-1 hash of the blob's contents.
+	HexSha1Hash() string
+}
