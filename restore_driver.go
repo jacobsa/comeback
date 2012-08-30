5908426d661ccf018670f959da2e572ef2b326e7
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-08-30 16:04:11 +1000

    Implemented main.

--- restore_driver.go
+++ restore_driver.go
@@ -16,18 +16,110 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jacobsa/comeback/blob"
+	"github.com/jacobsa/comeback/disk"
+	"github.com/jacobsa/comeback/fs"
 	"github.com/jacobsa/comeback/repr"
 	"log"
+	"os"
+	"path"
+)
+
+const (
+	gTarget = "/tmp/restore_target"
 )
 
 var blobStore blob.Store
 
+func fromHexHash(hexHash string) (blob.Score, error) {
+	return nil, fmt.Errorf("TODO: fromHexHash")
+}
+
+// Restore the file whose contents are described by the referenced blobs to the
+// supplied target, whose parent must already exist.
+func restoreFile(target string, scores []blob.Score) error {
+	return fmt.Errorf("TODO: restoreFile")
+}
+
 // Restore the directory whose contents are described by the referenced blob to
 // the supplied target, which must already exist.
-func restoreDir(target string, hexHash string) error {
+func restoreDir(target string, score blob.Score) error {
 	// Load the appropriate blob.
+	blob, err := blobStore.Load(score)
+	if err != nil {
+		return fmt.Errorf("Loading blob: %v", err)
+	}
+
+	// Parse its contents.
+	entries, err := repr.Unmarshal(blob)
+	if err != nil {
+		return fmt.Errorf("Parsing blob: %v", err)
+	}
+
+	// Deal with each entry.
+	for _, entry := range entries {
+		entryPath := path.Join(target, entry.Name)
+
+		// Switch on type.
+		switch entry.Type {
+		case fs.TypeFile:
+			err = restoreFile(entryPath, entry.Scores)
+			if err != nil {
+				return err
+			}
+
+		case fs.TypeDirectory:
+			if len(entry.Scores) != 1 {
+				return fmt.Errorf("Wrong number of scores: %v", entry)
+			}
+
+			if err = os.Mkdir(entryPath, 0700); err != nil {
+				return err
+			}
+
+			if err = restoreDir(entryPath, entry.Scores[0]); err != nil {
+				return err
+			}
+
+		default:
+			return fmt.Errorf("Don't know how to deal with entry: %v", entry)
+		}
+	}
+
+	return nil
 }
 
 func main() {
+	var err error
+
+	// Create the blob store.
+	blobStore, err = disk.NewBlobStore("/tmp/blobs")
+	if err != nil {
+		log.Fatalf("Creating store: %v", err)
+	}
+
+	// Parse the score.
+	score, err := fromHexHash("ee905ccc3416d120c962dc983023c28401ae39ae")
+	if err != nil {
+		log.Fatalf("Parsing score: %v", err)
+	}
+
+	// Make sure the target doesn't exist.
+	err = os.RemoveAll(gTarget)
+	if err != nil {
+		log.Fatalf("RemoveAll: %v", err)
+	}
+
+	// Create the target.
+	err = os.Mkdir("/tmp/restore_target", 0700)
+	if err != nil {
+		log.Fatalf("Mkdir: %v", err)
+	}
+
+	// Attempt a restore.
+	err = restoreDir(gTarget, score)
+	if err != nil {
+		log.Fatalf("Restoring: %v", err)
+	}
 }
