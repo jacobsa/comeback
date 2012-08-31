b7b1ebb5d18f1b5fa321fa89eabe5b1d561eb173
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-08-31 16:42:52 +1000

    Added a mock link resolver.

--- backup/mock/mock.go
+++ backup/mock/mock.go
@@ -7,11 +7,11 @@
 package mock_backup
 
 import (
-	fmt "fmt"
 	backup "github.com/jacobsa/comeback/backup"
 	blob "github.com/jacobsa/comeback/blob"
-	oglemock "github.com/jacobsa/oglemock"
+	fmt "fmt"
 	io "io"
+	oglemock "github.com/jacobsa/oglemock"
 	runtime "runtime"
 	unsafe "unsafe"
 )
@@ -22,16 +22,16 @@ type MockFileSaver interface {
 }
 
 type mockFileSaver struct {
-	controller  oglemock.Controller
-	description string
+	controller	oglemock.Controller
+	description	string
 }
 
 func NewMockFileSaver(
 	c oglemock.Controller,
 	desc string) MockFileSaver {
 	return &mockFileSaver{
-		controller:  c,
-		description: desc,
+		controller:	c,
+		description:	desc,
 	}
 }
 
@@ -78,16 +78,16 @@ type MockDirectorySaver interface {
 }
 
 type mockDirectorySaver struct {
-	controller  oglemock.Controller
-	description string
+	controller	oglemock.Controller
+	description	string
 }
 
 func NewMockDirectorySaver(
 	c oglemock.Controller,
 	desc string) MockDirectorySaver {
 	return &mockDirectorySaver{
-		controller:  c,
-		description: desc,
+		controller:	c,
+		description:	desc,
 	}
 }
 
@@ -123,6 +123,57 @@ func (m *mockDirectorySaver) Save(p0 string) (o0 blob.Score, o1 error) {
 	// o1 error
 	if retVals[1] != nil {
 		o1 = retVals[1].(error)
+	}
+
+	return
+}
+
+type MockLinkResolver interface {
+	backup.LinkResolver
+	oglemock.MockObject
+}
+
+type mockLinkResolver struct {
+	controller	oglemock.Controller
+	description	string
+}
+
+func NewMockLinkResolver(
+	c oglemock.Controller,
+	desc string) MockLinkResolver {
+	return &mockLinkResolver{
+		controller:	c,
+		description:	desc,
+	}
+}
+
+func (m *mockLinkResolver) Oglemock_Id() uintptr {
+	return uintptr(unsafe.Pointer(m))
+}
+
+func (m *mockLinkResolver) Oglemock_Description() string {
+	return m.description
+}
+
+func (m *mockLinkResolver) Register(p0 int32, p1 uint64, p2 string) (o0 *string) {
+	// Get a file name and line number for the caller.
+	_, file, line, _ := runtime.Caller(1)
+
+	// Hand the call off to the controller, which does most of the work.
+	retVals := m.controller.HandleMethodCall(
+		m,
+		"Register",
+		file,
+		line,
+		[]interface{}{p0, p1, p2})
+
+	if len(retVals) != 1 {
+		panic(fmt.Sprintf("mockLinkResolver.Register: invalid return values: %v", retVals))
+	}
+
+	// o0 *string
+	if retVals[0] != nil {
+		o0 = retVals[0].(*string)
 	}
 
 	return
