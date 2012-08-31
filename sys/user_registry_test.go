a8e2d380017c95551cb0a87e76a05958e54da736
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-08-31 13:47:31 +1000

    Added a test stub.

---
+++ sys/user_registry_test.go
@@ -0,0 +1,47 @@
+// Copyright 2012 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sys_test
+
+import (
+	"github.com/jacobsa/comeback/sys"
+	. "github.com/jacobsa/oglematchers"
+	. "github.com/jacobsa/ogletest"
+	"log"
+	"testing"
+)
+
+func TestUserRegistry(t *testing.T) { RunTests(t) }
+
+////////////////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////////////////
+
+type UserRegistryTest struct {
+	registry sys.UserRegistry
+}
+
+func init() { RegisterTestSuite(&UserRegistryTest{}) }
+
+func (t *UserRegistryTest) SetUp(i *TestInfo) {
+	var err error
+	if t.registry, err = sys.NewUserRegistry(); err != nil {
+		log.Fatalf("Creating registry: %v", err)
+	}
+}
+
+func (t *UserRegistryTest) DoesFoo() {
+	ExpectEq("TODO", "")
+}
