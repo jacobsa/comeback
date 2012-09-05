f53c8b0845e25e573b5cddb79ff890a3ae28c83a
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-09-05 15:26:44 +1000

    Made blob/mock build again.

--- blob/mock/store.go
+++ blob/mock/store.go
@@ -7,63 +7,12 @@
 package mock_blob
 
 import (
-	fmt "fmt"
 	blob "github.com/jacobsa/comeback/blob"
+	fmt "fmt"
 	oglemock "github.com/jacobsa/oglemock"
 	runtime "runtime"
 	unsafe "unsafe"
 )
-
-type MockScore interface {
-	blob.Score
-	oglemock.MockObject
-}
-
-type mockScore struct {
-	controller  oglemock.Controller
-	description string
-}
-
-func NewMockScore(
-	c oglemock.Controller,
-	desc string) MockScore {
-	return &mockScore{
-		controller:  c,
-		description: desc,
-	}
-}
-
-func (m *mockScore) Oglemock_Id() uintptr {
-	return uintptr(unsafe.Pointer(m))
-}
-
-func (m *mockScore) Oglemock_Description() string {
-	return m.description
-}
-
-func (m *mockScore) Sha1Hash() (o0 []uint8) {
-	// Get a file name and line number for the caller.
-	_, file, line, _ := runtime.Caller(1)
-
-	// Hand the call off to the controller, which does most of the work.
-	retVals := m.controller.HandleMethodCall(
-		m,
-		"Sha1Hash",
-		file,
-		line,
-		[]interface{}{})
-
-	if len(retVals) != 1 {
-		panic(fmt.Sprintf("mockScore.Sha1Hash: invalid return values: %v", retVals))
-	}
-
-	// o0 []uint8
-	if retVals[0] != nil {
-		o0 = retVals[0].([]uint8)
-	}
-
-	return
-}
 
 type MockStore interface {
 	blob.Store
@@ -71,16 +20,16 @@ type MockStore interface {
 }
 
 type mockStore struct {
-	controller  oglemock.Controller
-	description string
+	controller	oglemock.Controller
+	description	string
 }
 
 func NewMockStore(
 	c oglemock.Controller,
 	desc string) MockStore {
 	return &mockStore{
-		controller:  c,
-		description: desc,
+		controller:	c,
+		description:	desc,
 	}
 }
 
