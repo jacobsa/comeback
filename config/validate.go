96af3c6948c96e8467db9cc22bb31767dab71951
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-08 15:09:02 +1100

    Implemented some of Validate.

--- config/validate.go
+++ config/validate.go
@@ -17,9 +17,32 @@ package config
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
+
+func validateJob(j *Job) error {
+	// Base paths must be non-empty valid UTF-8.
+	if j.BasePath == "" || !utf8.Valid([]byte(j.BasePath)) {
+		return fmt.Errorf("Base paths must be non-empty valid UTF-8.")
+	}
+
+	return nil
+}
 
 // Return an error if the supplied config data is invalid in some way.
 func Validate(c *Config) error {
-	return fmt.Errorf("TODO: Implement config.Validate.")
+	// Check each job.
+	for name, job := range c.Jobs {
+		// Names must be valid UTF-8.
+		if !utf8.Valid([]byte(name)) {
+			return fmt.Errorf("Job names must be valid UTF-8.")
+		}
+
+		// Check the job itself.
+		if err := validateJob(job); err != nil {
+			return fmt.Errorf("Job %s: %v", name, err)
+		}
+	}
+
+	return nil
 }
