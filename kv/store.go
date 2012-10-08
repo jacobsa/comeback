4d8148e89a1d7bfc4f841c11c912ea15fdb81a79
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-08 15:33:25 +1100

    Defined a kv-store interface.

---
+++ kv/store.go
@@ -0,0 +1,31 @@
+// Copyright 2012 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kb
+
+// A Store knows how to store keys keyed on values. The format of keys and
+// values is left unspecified.
+type Store interface {
+	// Set the supplied value for the given key, overwriting any existing value.
+	Set(key []byte, val []byte) error
+
+	// Get the most recent value for the supplied key, returning an error if the
+	// key is unknown.
+	Get(key []byte) (val []byte, err error)
+
+	// Return true only if the store contains a value for the supplied key. If it
+	// is unknown whether the key exists, the store is permitted to return false.
+	Contains(key []byte) (res bool, err error)
+}
