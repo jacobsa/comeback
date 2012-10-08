098765ac580ca8490174bc402b31f3f8ca1a5c46
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-08 12:29:01 +1100

    Fixed package name.

--- config/types.go
+++ config/types.go
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package backup
+package config
 
 type Job struct {
 	// The path on the file system that should be backed up.
