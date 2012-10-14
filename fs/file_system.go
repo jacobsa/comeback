109c78655535983462fa5444874520ae5c2678c4
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-14 20:25:35 +1100

    Split ReadDir into a separate file.

--- fs/file_system.go
+++ fs/file_system.go
@@ -17,18 +17,10 @@
 package fs
 
 import (
-	"fmt"
 	"github.com/jacobsa/comeback/sys"
 	"io"
 	"io/ioutil"
 	"os"
-	"path"
-	"syscall"
-)
-
-const (
-	// A permissions bit mask that matches chmod(2)'s notion of permissions.
-	permissionBits os.FileMode = os.ModePerm | os.ModeSetuid | os.ModeSetgid | os.ModeSticky
 )
 
 // FileSystem represents operations performed on a real file system, but is an
@@ -58,99 +50,6 @@ func NewFileSystem(
 type fileSystem struct {
 	userRegistry  sys.UserRegistry
 	groupRegistry sys.GroupRegistry
-}
-
-func (fs *fileSystem) convertFileInfo(fi os.FileInfo) (entry *DirectoryEntry, err error) {
-	// Grab system-specific info.
-	statT, ok := fi.Sys().(*syscall.Stat_t)
-	if !ok {
-		return nil, fmt.Errorf("Unexpected sys value: %v", fi.Sys())
-	}
-
-	// Create the basic entry.
-	entry = &DirectoryEntry{
-		Permissions: fi.Mode() & permissionBits,
-		Name:        fi.Name(),
-		MTime:       fi.ModTime(),
-		Uid:         sys.UserId(statT.Uid),
-		Gid:         sys.GroupId(statT.Gid),
-	}
-
-	// Attempt to look up user info.
-	username, err := fs.userRegistry.FindById(entry.Uid)
-
-	if _, ok := err.(sys.NotFoundError); ok {
-		err = nil
-	} else if err != nil {
-		return nil, fmt.Errorf("Looking up user: %v", err)
-	} else {
-		entry.Username = &username
-	}
-
-	// Attempt to look up group info.
-	groupname, err := fs.groupRegistry.FindById(entry.Gid)
-
-	if _, ok := err.(sys.NotFoundError); ok {
-		err = nil
-	} else if err != nil {
-		return nil, fmt.Errorf("Looking up group: %v", err)
-	} else {
-		entry.Groupname = &groupname
-	}
-
-	// Convert the type.
-	typeBits := fi.Mode() & (os.ModeType | os.ModeCharDevice)
-	switch typeBits {
-	case 0:
-		entry.Type = TypeFile
-		entry.ContainingDevice = statT.Dev
-		entry.Inode = statT.Ino
-	case os.ModeDir:
-		entry.Type = TypeDirectory
-	case os.ModeSymlink:
-		entry.Type = TypeSymlink
-	case os.ModeDevice:
-		entry.Type = TypeBlockDevice
-		entry.DeviceNumber = statT.Rdev
-	case os.ModeDevice | os.ModeCharDevice:
-		entry.Type = TypeCharDevice
-		entry.DeviceNumber = statT.Rdev
-	case os.ModeNamedPipe:
-		entry.Type = TypeNamedPipe
-	default:
-		return entry, fmt.Errorf("Unhandled mode: %v %u", fi.Mode(), fi.Mode())
-	}
-
-	return entry, nil
-}
-
-func (fs *fileSystem) ReadDir(dirpath string) (entries []*DirectoryEntry, err error) {
-	// Call ioutil.
-	fileInfos, err := ioutil.ReadDir(dirpath)
-	if err != nil {
-		return nil, err
-	}
-
-	// Convert each entry.
-	entries = []*DirectoryEntry{}
-	for _, fileInfo := range fileInfos {
-		entry, err := fs.convertFileInfo(fileInfo)
-		if err != nil {
-			return nil, err
-		}
-
-		// Handle symlinks.
-		if entry.Type == TypeSymlink {
-			linkPath := path.Join(dirpath, entry.Name)
-			if entry.Target, err = os.Readlink(linkPath); err != nil {
-				return nil, err
-			}
-		}
-
-		entries = append(entries, entry)
-	}
-
-	return entries, nil
 }
 
 func (f *fileSystem) OpenForReading(path string) (r io.ReadCloser, err error) {
