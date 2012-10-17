97a6917a7b24241f778af585e6e565fac4da13eb
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-17 21:55:17 +1100

    Updated mock.

--- fs/mock/file_system.go
+++ fs/mock/file_system.go
@@ -44,6 +44,30 @@ func (m *mockFileSystem) Oglemock_Description() string {
 	return m.description
 }
 
+func (m *mockFileSystem) Chown(p0 string, p1 int, p2 int) (o0 error) {
+	// Get a file name and line number for the caller.
+	_, file, line, _ := runtime.Caller(1)
+
+	// Hand the call off to the controller, which does most of the work.
+	retVals := m.controller.HandleMethodCall(
+		m,
+		"Chown",
+		file,
+		line,
+		[]interface{}{p0, p1, p2})
+
+	if len(retVals) != 1 {
+		panic(fmt.Sprintf("mockFileSystem.Chown: invalid return values: %v", retVals))
+	}
+
+	// o0 error
+	if retVals[0] != nil {
+		o0 = retVals[0].(error)
+	}
+
+	return
+}
+
 func (m *mockFileSystem) CreateBlockDevice(p0 string, p1 os.FileMode, p2 int32) (o0 error) {
 	// Get a file name and line number for the caller.
 	_, file, line, _ := runtime.Caller(1)
@@ -121,6 +145,30 @@ func (m *mockFileSystem) CreateFile(p0 string, p1 os.FileMode) (o0 io.WriteClose
 	return
 }
 
+func (m *mockFileSystem) CreateHardLink(p0 string, p1 string) (o0 error) {
+	// Get a file name and line number for the caller.
+	_, file, line, _ := runtime.Caller(1)
+
+	// Hand the call off to the controller, which does most of the work.
+	retVals := m.controller.HandleMethodCall(
+		m,
+		"CreateHardLink",
+		file,
+		line,
+		[]interface{}{p0, p1})
+
+	if len(retVals) != 1 {
+		panic(fmt.Sprintf("mockFileSystem.CreateHardLink: invalid return values: %v", retVals))
+	}
+
+	// o0 error
+	if retVals[0] != nil {
+		o0 = retVals[0].(error)
+	}
+
+	return
+}
+
 func (m *mockFileSystem) CreateNamedPipe(p0 string, p1 os.FileMode) (o0 error) {
 	// Get a file name and line number for the caller.
 	_, file, line, _ := runtime.Caller(1)
@@ -135,6 +183,54 @@ func (m *mockFileSystem) CreateNamedPipe(p0 string, p1 os.FileMode) (o0 error) {
 
 	if len(retVals) != 1 {
 		panic(fmt.Sprintf("mockFileSystem.CreateNamedPipe: invalid return values: %v", retVals))
+	}
+
+	// o0 error
+	if retVals[0] != nil {
+		o0 = retVals[0].(error)
+	}
+
+	return
+}
+
+func (m *mockFileSystem) CreateSymlink(p0 string, p1 string, p2 os.FileMode) (o0 error) {
+	// Get a file name and line number for the caller.
+	_, file, line, _ := runtime.Caller(1)
+
+	// Hand the call off to the controller, which does most of the work.
+	retVals := m.controller.HandleMethodCall(
+		m,
+		"CreateSymlink",
+		file,
+		line,
+		[]interface{}{p0, p1, p2})
+
+	if len(retVals) != 1 {
+		panic(fmt.Sprintf("mockFileSystem.CreateSymlink: invalid return values: %v", retVals))
+	}
+
+	// o0 error
+	if retVals[0] != nil {
+		o0 = retVals[0].(error)
+	}
+
+	return
+}
+
+func (m *mockFileSystem) Mkdir(p0 string, p1 os.FileMode) (o0 error) {
+	// Get a file name and line number for the caller.
+	_, file, line, _ := runtime.Caller(1)
+
+	// Hand the call off to the controller, which does most of the work.
+	retVals := m.controller.HandleMethodCall(
+		m,
+		"Mkdir",
+		file,
+		line,
+		[]interface{}{p0, p1})
+
+	if len(retVals) != 1 {
+		panic(fmt.Sprintf("mockFileSystem.Mkdir: invalid return values: %v", retVals))
 	}
 
 	// o0 error
