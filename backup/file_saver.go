faf26b3bb9ec0dfdedbfca79f138211d56d10d13
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-22 12:10:57 +1100

    Switched to using an executor.

--- backup/file_saver.go
+++ backup/file_saver.go
@@ -18,9 +18,9 @@ package backup
 import (
 	"fmt"
 	"github.com/jacobsa/comeback/blob"
+	"github.com/jacobsa/comeback/concurrent"
 	"io"
 	"io/ioutil"
-	"runtime"
 )
 
 // An object that knows how to save files to some underlying storage.
@@ -33,53 +33,25 @@ type FileSaver interface {
 
 // Create a file saver that uses the supplied blob store, splitting files into
 // chunks of the specified size.
-func NewFileSaver(store blob.Store, chunkSize uint32) (FileSaver, error) {
+func NewFileSaver(store blob.Store, chunkSize uint32) (s FileSaver, err error) {
 	if chunkSize == 0 {
 		return nil, fmt.Errorf("Chunk size must be positive.")
 	}
 
-	saver := &fileSaver{blobStore: store, chunkSize: chunkSize}
-	startWorkers(saver)
-	runtime.SetFinalizer(saver, stopWorkers)
+	const numWorkers = 1
+	s = &fileSaver{
+		store,
+		chunkSize,
+		concurrent.NewExecutor(numWorkers),
+	}
 
-	return saver, nil
-}
-
-type fileSaverResult struct {
-	score blob.Score
-	err error
-}
-
-type fileSaverWork struct {
-	data []byte
-	resultChan chan<- fileSaverResult
+	return
 }
 
 type fileSaver struct {
 	blobStore blob.Store
 	chunkSize uint32
-	work chan<- fileSaverWork
-}
-
-func startWorkers(s *fileSaver) {
-	work := make(chan fileSaverWork)
-	s.work = work
-
-	processWork := func() {
-		for item := range work {
-			score, err := s.blobStore.Store(item.data)
-			item.resultChan <- fileSaverResult{score, err}
-		}
-	}
-
-	const numWorkers = 1
-	for i := 0; i < numWorkers; i++ {
-		go processWork()
-	}
-}
-
-func stopWorkers(s *fileSaver) {
-	close(s.work)
+	executor concurrent.Executor
 }
 
 // Read 16 MiB from the supplied reader, returning less iff the reader returns
@@ -90,10 +62,16 @@ func getChunk(r io.Reader, chunkSize uint32) ([]byte, error) {
 }
 
 func (s *fileSaver) Save(r io.Reader) (scores []blob.Score, err error) {
-	// Turn the file into chunks, giving each to the saver's workers to store in
-	// the blob store.
-	resultChans := make([]<-chan fileSaverResult, 0)
+	type result struct {
+		score blob.Score
+		err error
+	}
+
+	// Turn the file into chunks, giving them to the blob store in parallel.
+	resultChans := make([]<-chan result, 0)
+
 	for {
+		// Read the chunk.
 		var chunk []byte
 		chunk, err = getChunk(r, s.chunkSize)
 		if err != nil {
@@ -107,13 +85,18 @@ func (s *fileSaver) Save(r io.Reader) (scores []blob.Score, err error) {
 		}
 
 		// Add the chunk to the queue of work. Make sure to use a buffered channel
-		// for the result so that the workers don't block on sending to it while we
-		// block on sending to them.
-		resultChan := make(chan fileSaverResult, 1)
+		// for the result so that the work function doesn't block on sending to it
+		// while we block on sending to the executor running the work function.
+		resultChan := make(chan result, 1)
 		resultChans = append(resultChans, resultChan)
 
-		workItem := fileSaverWork{chunk, resultChan}
-		s.work <- workItem
+		processChunk := func() {
+			var r result
+			r.score, r.err = s.blobStore.Store(chunk)
+			resultChan <- r
+		}
+
+		s.executor.Add(processChunk)
 	}
 
 	// Read back scores.
