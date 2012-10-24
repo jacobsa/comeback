e0dc305ae669342598aa6954a9e7cae662fe38db
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-24 23:52:13 +1100

    Added a State type.

---
+++ state/state.go
@@ -0,0 +1,38 @@
+// Copyright 2012 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package state
+
+import (
+	"io"
+	"github.com/jacobsa/comeback/blob"
+)
+
+// State that should be saved between runs of the program.
+type State struct {
+	// The set of scores that are known to already exist in the blob store. It is
+	// unnecessary to again store any blob with one of these scores.
+	ExistingScores ScoreSet
+
+	// A version number for the above score set. Any time state is saved, this
+	// version should first be updated to a random number and saved to the backup
+	// registry, making sure that the old version was still the current one. This
+	// protects us from drifting out of date if another process is concurrently
+	// adding scores to the blob store.
+	ExistingScoresVersion uint64
+}
+
+func LoadState(r io.Reader) (state State, err error)
+func SaveState(w io.Writer, state State) state
