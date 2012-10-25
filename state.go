3a7d8c3d93500c4fcb022c1f384493e9f467a5f4
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-25 23:10:24 +1100

    Added a getState function.

---
+++ state.go
@@ -0,0 +1,50 @@
+// Copyright 2012 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/jacobsa/comeback/state"
+	"log"
+	"os"
+	"sync"
+)
+
+var g_stateOnce sync.Once
+var g_state state.State
+
+func initState() {
+	cfg := getConfig()
+	var err error
+
+	// Open the specified file.
+	f, err := os.Open(cfg.StateFile)
+	if err != nil {
+		log.Fatalln("Opening state file:", err)
+	}
+
+	defer f.close()
+
+	// Load the state struct.
+	g_state, err = state.LoadState(f)
+	if err != nil {
+		log.Fatalln("LoadState:", err)
+	}
+}
+
+func getState() state.State {
+	g_stateOnce.Do(initState)
+	return g_state
+}
