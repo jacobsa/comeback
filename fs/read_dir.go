b30864383f39f9a07e18ef80942f6d8ae568b2cb
Aaron Jacobs <aaronjjacobs@gmail.com>
2012-10-29 23:04:29 +1100

    Moved some helpers to a separate file.

--- fs/read_dir.go
+++ fs/read_dir.go
@@ -16,89 +16,10 @@
 package fs
 
 import (
-	"fmt"
-	"github.com/jacobsa/comeback/sys"
 	"io/ioutil"
 	"os"
 	"path"
-	"syscall"
 )
-
-const (
-	// A permissions bit mask that matches chmod(2)'s notion of permissions.
-	permissionBits os.FileMode = os.ModePerm | os.ModeSetuid | os.ModeSetgid | os.ModeSticky
-)
-
-func (fs *fileSystem) convertFileInfo(fi os.FileInfo) (entry *DirectoryEntry, err error) {
-	// Grab system-specific info.
-	statT, ok := fi.Sys().(*syscall.Stat_t)
-	if !ok {
-		return nil, fmt.Errorf("Unexpected sys value: %v", fi.Sys())
-	}
-
-	// Create the basic entry.
-	entry = &DirectoryEntry{
-		Permissions: fi.Mode() & permissionBits,
-		Name:        fi.Name(),
-		MTime:       fi.ModTime(),
-		Uid:         sys.UserId(statT.Uid),
-		Gid:         sys.GroupId(statT.Gid),
-	}
-
-	// Attempt to look up user info.
-	username, err := fs.userRegistry.FindById(entry.Uid)
-
-	if _, ok := err.(sys.NotFoundError); ok {
-		err = nil
-	} else if err != nil {
-		return nil, fmt.Errorf("Looking up user: %v", err)
-	} else {
-		entry.Username = &username
-	}
-
-	// Attempt to look up group info.
-	groupname, err := fs.groupRegistry.FindById(entry.Gid)
-
-	if _, ok := err.(sys.NotFoundError); ok {
-		err = nil
-	} else if err != nil {
-		return nil, fmt.Errorf("Looking up group: %v", err)
-	} else {
-		entry.Groupname = &groupname
-	}
-
-	// Convert the type.
-	typeBits := fi.Mode() & (os.ModeType | os.ModeCharDevice)
-	switch typeBits {
-	case 0:
-		entry.Type = TypeFile
-		entry.ContainingDevice = statT.Dev
-		entry.Inode = statT.Ino
-
-		if statT.Size < 0 {
-			panic(fmt.Sprintf("Unexpected size: %d", statT.Size))
-		}
-
-		entry.Size = uint64(statT.Size)
-
-	case os.ModeDir:
-		entry.Type = TypeDirectory
-	case os.ModeSymlink:
-		entry.Type = TypeSymlink
-	case os.ModeDevice:
-		entry.Type = TypeBlockDevice
-		entry.DeviceNumber = statT.Rdev
-	case os.ModeDevice | os.ModeCharDevice:
-		entry.Type = TypeCharDevice
-		entry.DeviceNumber = statT.Rdev
-	case os.ModeNamedPipe:
-		entry.Type = TypeNamedPipe
-	default:
-		return entry, fmt.Errorf("Unhandled mode: %v %u", fi.Mode(), fi.Mode())
-	}
-
-	return entry, nil
-}
 
 func (fs *fileSystem) ReadDir(dirpath string) (entries []*DirectoryEntry, err error) {
 	// Call ioutil.
