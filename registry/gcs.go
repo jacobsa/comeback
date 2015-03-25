d2ca06ffd9786c2db0bccdb14e71213e3f4789a8
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-03-26 08:07:29 +1100

    Added comments about the marker item.

--- registry/gcs.go
+++ registry/gcs.go
@@ -41,15 +41,6 @@ func NewGCSRegistry(
 		rand.Reader)
 }
 
-// Like NewGCSRegistry, but with more injected.
-func newGCSRegistry(
-	bucket gcs.Bucket,
-	cryptoPassword string,
-	deriver crypto.KeyDeriver,
-	createCrypter func(key []byte) (crypto.Crypter, error),
-	cryptoRandSrc io.Reader) (r Registry, crypter crypto.Crypter, err error) {
-}
-
 const (
 	gcsJobKeyPrefix      = "jobs/"
 	gcsMetadataKey_Name  = "job_name"
@@ -65,8 +56,24 @@ const (
 // time.RFC3339Nano. Additional information is stored as object metadata fields
 // keyed by the constants above. Metadata fields are used in preference to
 // object content so that they are accessible on a ListObjects request.
+//
+// The bucket additionally contains a "marker" object (named by the constant
+// markerItemName) with metadata keys specifying a salt and a ciphertext for
+// some random plaintext, generated ant written the first time the bucket is
+// used. This marker allows us to verify that the user-provided crypto password
+// is correct by deriving a key using the password and the salt and making sure
+// that the ciphertext can be decrypted using that key.
 type gcsRegistry struct {
 	bucket gcs.Bucket
+}
+
+// Like NewGCSRegistry, but with more injected.
+func newGCSRegistry(
+	bucket gcs.Bucket,
+	cryptoPassword string,
+	deriver crypto.KeyDeriver,
+	createCrypter func(key []byte) (crypto.Crypter, error),
+	cryptoRandSrc io.Reader) (r Registry, crypter crypto.Crypter, err error) {
 }
 
 func (r *gcsRegistry) RecordBackup(j CompletedJob) (err error) {
