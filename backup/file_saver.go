7da6dea7f74e1aa51e27da21701700a846269fc8
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-03-29 15:02:31 +1100

    Ripped out ineffective parallelism.
    Parallelism within a single file isn't really what we want. Instead we
    want to decouple the file scanning (which can run on one CPU at disk
    rate, no need to parallelize) from GCS writes (which need to be
    parallelized due to latency overhead of each request).

--- backup/file_saver.go
+++ backup/file_saver.go
@@ -21,7 +21,6 @@ import (
 	"io/ioutil"
 
 	"github.com/jacobsa/comeback/blob"
-	"github.com/jacobsa/comeback/concurrent"
 	"github.com/jacobsa/comeback/fs"
 	"github.com/jacobsa/comeback/repr"
 )
@@ -35,15 +34,11 @@ type FileSaver interface {
 }
 
 // Create a file saver that uses the supplied blob store, splitting files into
-// chunks of the specified size. The executor will be used for scheduling calls
-// to the blob store, and may be used to control the degree of parallelism in
-// those calls.
+// chunks of the specified size.
 func NewFileSaver(
 	store blob.Store,
 	chunkSize uint32,
-	fileSystem fs.FileSystem,
-	executor concurrent.Executor,
-) (s FileSaver, err error) {
+	fileSystem fs.FileSystem) (s FileSaver, err error) {
 	if chunkSize == 0 {
 		return nil, fmt.Errorf("Chunk size must be positive.")
 	}
@@ -52,7 +47,6 @@ func NewFileSaver(
 		store,
 		chunkSize,
 		fileSystem,
-		executor,
 	}
 
 	return
@@ -62,7 +56,6 @@ type fileSaver struct {
 	blobStore  blob.Store
 	chunkSize  uint32
 	fileSystem fs.FileSystem
-	executor   concurrent.Executor
 }
 
 // Read 16 MiB from the supplied reader, returning less iff the reader returns
@@ -83,22 +76,7 @@ func (s *fileSaver) Save(path string) (scores []blob.Score, err error) {
 
 	defer file.Close()
 
-	// Turn the file into chunks, giving them to the blob store in parallel.
-	type result struct {
-		score blob.Score
-		err   error
-	}
-
-	resultChans := make([]<-chan result, 0)
-
-	// Make sure we drain results before returning, even if we return early. This
-	// makes it easy to avoid races in tests.
-	defer func() {
-		for _, c := range resultChans {
-			<-c
-		}
-	}()
-
+	// Turn the file into chunks, giving them to the blob store one by one.
 	for {
 		// Read the chunk.
 		var chunk []byte
@@ -113,51 +91,22 @@ func (s *fileSaver) Save(path string) (scores []blob.Score, err error) {
 			break
 		}
 
-		// Add the chunk to the queue of work. Make sure to use a buffered channel
-		// for the result so that the work function doesn't block on sending to it
-		// while we block on sending to the executor running the work function.
-		resultChan := make(chan result, 1)
-		resultChans = append(resultChans, resultChan)
-
-		processChunk := func() {
-			var r result
-
-			// Write the result when we're done.
-			defer func() {
-				resultChan <- r
-				close(resultChan)
-			}()
-
-			// Marshal the chunk.
-			var blob []byte
-			blob, r.err = repr.MarshalFile(chunk)
-			if r.err != nil {
-				r.err = fmt.Errorf("MarshalFile: %v", r.err)
-				return
-			}
-
-			// Write out the blob.
-			r.score, r.err = s.blobStore.Store(blob)
-			if r.err != nil {
-				r.err = fmt.Errorf("blobStore.Store: %v", r.err)
-				return
-			}
-		}
-
-		s.executor.Add(processChunk)
-	}
-
-	// Read back scores.
-	scores = make([]blob.Score, len(resultChans))
-
-	for i, resultChan := range resultChans {
-		result := <-resultChan
-		if result.err != nil {
-			err = fmt.Errorf("Storing chunk %d: %v", i, result.err)
+		// Encapsulate the chunk such that it can be identified as a file.
+		chunk, err = repr.MarshalFile(chunk)
+		if err != nil {
+			err = fmt.Errorf("MarshalFile: %v", err)
 			return
 		}
 
-		scores[i] = result.score
+		// Feed the chunk to the blob store.
+		var score blob.Score
+		score, err = s.blobStore.Store(chunk)
+		if err != nil {
+			err = fmt.Errorf("Store: %v", err)
+			return
+		}
+
+		scores = append(scores, score)
 	}
 
 	return
