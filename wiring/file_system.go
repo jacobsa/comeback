df8dd4e73b6af27eb19bcbc5cfa6918b123e519d
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-05-10 15:14:47 +1000

    makeFileSystem

--- wiring/file_system.go
+++ wiring/file_system.go
@@ -15,7 +15,35 @@
 
 package wiring
 
-import "github.com/jacobsa/comeback/fs"
+import (
+	"fmt"
+
+	"github.com/jacobsa/comeback/fs"
+	"github.com/jacobsa/comeback/sys"
+)
 
 // Create a FileSystem that writes to the real file system.
-func makeFileSystem() (fs fs.FileSystem, err error)
+func makeFileSystem() (fileSystem fs.FileSystem, err error) {
+	// Create a user registry.
+	userRegistry, err := sys.NewUserRegistry()
+	if err != nil {
+		err = fmt.Errorf("NewUserRegistry: %v", err)
+		return
+	}
+
+	// Create a group registry.
+	groupRegistry, err := sys.NewGroupRegistry()
+	if err != nil {
+		err = fmt.Errorf("NewGroupRegistry: %v", err)
+		return
+	}
+
+	// Create the file system.
+	fileSystem, err = fs.NewFileSystem(userRegistry, groupRegistry)
+	if err != nil {
+		err = fmt.Errorf("NewFileSystem: %v", err)
+		return
+	}
+
+	return
+}
