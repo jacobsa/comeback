06277505d2a174ae84681edb23aa1befc7e1da02
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-05-13 16:36:02 +1000

    Reuse for buildExistingScores.

--- state.go
+++ state.go
@@ -25,12 +25,10 @@ import (
 
 	"golang.org/x/net/context"
 
-	"github.com/jacobsa/comeback/blob"
 	"github.com/jacobsa/comeback/state"
 	"github.com/jacobsa/comeback/util"
 	"github.com/jacobsa/comeback/wiring"
 	"github.com/jacobsa/gcloud/gcs"
-	"github.com/jacobsa/gcloud/syncutil"
 )
 
 var g_stateOnce sync.Once
@@ -40,31 +38,22 @@ var g_saveStateMutex sync.Mutex
 
 func buildExistingScores(
 	bucket gcs.Bucket) (existingScores util.StringSet, err error) {
-	b := syncutil.NewBundle(context.Background())
-	defer func() { err = b.Join() }()
+	// List into a slice.
+	slice, err := listAllScores(
+		context.Background(),
+		bucket,
+		wiring.BlobObjectNamePrefix)
 
-	// List scores into a channel.
-	scores := make(chan blob.Score, 100)
-	b.Add(func(ctx context.Context) (err error) {
-		defer close(scores)
-		err = blob.ListScores(ctx, bucket, wiring.BlobObjectNamePrefix, scores)
-		if err != nil {
-			err = fmt.Errorf("ListScores: %v", err)
-			return
-		}
-
+	if err != nil {
+		err = fmt.Errorf("listAllScores: %v", err)
 		return
-	})
+	}
 
-	// Accumulate hex scores into a string set.
+	// Build a set.
 	existingScores = util.NewStringSet()
-	b.Add(func(ctx context.Context) (err error) {
-		for score := range scores {
-			existingScores.Add(score.Hex())
-		}
-
-		return
-	})
+	for _, score := range slice {
+		existingScores.Add(score.Hex())
+	}
 
 	return
 }
