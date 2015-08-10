754c73e64a3178f21f87b0c7963899243c738b7e
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-08-10 14:19:35 +1000

    dirHandle.ensureListing

--- internal/comebackfs/dir_handle.go
+++ internal/comebackfs/dir_handle.go
@@ -17,11 +17,15 @@ package comebackfs
 
 import (
 	"errors"
+	"fmt"
 
 	"golang.org/x/net/context"
 
 	"github.com/jacobsa/comeback/internal/blob"
+	"github.com/jacobsa/comeback/internal/fs"
+	"github.com/jacobsa/comeback/internal/repr"
 	"github.com/jacobsa/fuse/fuseops"
+	"github.com/jacobsa/fuse/fuseutil"
 	"github.com/jacobsa/syncutil"
 )
 
@@ -51,9 +55,82 @@ type dirHandle struct {
 	/////////////////////////
 
 	mu syncutil.InvariantMutex
+
+	// A listing of the directory, or nil if we haven't yet read the blob.
+	//
+	// GUARDED_BY(mu)
+	listing []fuseutil.Dirent
 }
 
 func (dh *dirHandle) checkInvariants() {
+}
+
+// Ensure that dh.listing is non-nil.
+//
+// LOCKS_REQUIRED(dh)
+func (dh *dirHandle) ensureListing(ctx context.Context) (err error) {
+	// Read the blob.
+	contents, err := dh.blobStore.Load(ctx, dh.score)
+	if err != nil {
+		err = fmt.Errorf("blobStore.Load: %v", err)
+		return
+	}
+
+	// Parse it.
+	entries, err := repr.UnmarshalDir(contents)
+	if err != nil {
+		err = fmt.Errorf("UnmarshalDir: %v", err)
+		return
+	}
+
+	// Convert, ensuring that the result is a non-nil slice even if the listing
+	// is empty.
+	listing := make([]fuseutil.Dirent, 0, len(entries)+1)
+	for i, e := range entries {
+		// We don't support hard links.
+		if e.HardLinkTarget != nil {
+			err = errors.New("Hard link enountered.")
+			return
+		}
+
+		de := fuseutil.Dirent{
+			Offset: fuseops.DirOffset(i),
+			Name:   e.Name,
+			Type:   convertEntryType(e.Type),
+
+			// Return a bogus inode ID for each entry, but not the root inode ID.
+			//
+			// NOTE(jacobsa): As far as I can tell this is harmless. Minting and
+			// returning a real inode ID is difficult because fuse does not count
+			// readdir as an operation that increases the inode ID's lookup count and
+			// we therefore don't get a forget for it later, but we would like to not
+			// have to remember every inode ID that we've ever minted for readdir.
+			Inode: fuseops.RootInodeID + 1,
+		}
+
+		listing = append(listing, de)
+	}
+
+	// Store the result.
+	dh.listing = listing
+
+	return
+}
+
+func convertEntryType(t fs.EntryType) fuseutil.DirentType {
+	switch t {
+	case fs.TypeFile:
+		return fuseutil.DT_File
+
+	case fs.TypeDirectory:
+		return fuseutil.DT_Directory
+
+	case fs.TypeSymlink:
+		return fuseutil.DT_Link
+
+	default:
+		return fuseutil.DT_Unknown
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////
