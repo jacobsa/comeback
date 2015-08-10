2a1d1de41c219fdec2368efc8246032cf6537af4
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-08-10 16:23:50 +1000

    Declared fileHandle.

---
+++ internal/comebackfs/file_handle.go
@@ -0,0 +1,91 @@
+// Copyright 2015 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package comebackfs
+
+import (
+	"errors"
+	"os"
+
+	"golang.org/x/net/context"
+
+	"github.com/jacobsa/comeback/internal/blob"
+	"github.com/jacobsa/syncutil"
+)
+
+func newFileHandle(
+	scores []blob.Score,
+	blobStore blob.Store) (fh *fileHandle) {
+	fh = &fileHandle{
+		blobStore: blobStore,
+		scores:    scores,
+	}
+
+	fh.mu = syncutil.NewInvariantMutex(fh.checkInvariants)
+
+	return
+}
+
+////////////////////////////////////////////////////////////////////////
+// Internal
+////////////////////////////////////////////////////////////////////////
+
+type fileHandle struct {
+	blobStore blob.Store
+
+	/////////////////////////
+	// Constant data
+	/////////////////////////
+
+	scores []blob.Score
+
+	/////////////////////////
+	// Mutable data
+	/////////////////////////
+
+	mu syncutil.InvariantMutex
+
+	// A file containing the contents of the blobs with the above scores, or nil
+	// if they haven't yet been downloaded.
+	//
+	// GUARDED_BY(mu)
+	file *os.File
+}
+
+func (fh *fileHandle) checkInvariants() {
+}
+
+////////////////////////////////////////////////////////////////////////
+// Public interface
+////////////////////////////////////////////////////////////////////////
+
+// LOCKS_EXCLUDED(f)
+func (fh *fileHandle) Lock() {
+	f.mu.Lock()
+}
+
+// LOCKS_REQUIRED(f)
+func (fh *fileHandle) Unlock() {
+	f.mu.Unlock()
+}
+
+// Like io.ReaderAt, but with context support.
+func (fh *fileHandle) ReadAt(
+	ctx context.Context,
+	p []byte,
+	offset int64) (n int, err error) {
+	err = errors.New("TODO")
+	return
+}
