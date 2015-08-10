d125b1a3d7d05e2de982fe8c32ac4409a0e4d1bd
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-08-10 11:55:26 +1000

    Wrote much of the mount command.

--- mount.go
+++ mount.go
@@ -15,19 +15,93 @@
 
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jacobsa/comeback/internal/blob"
+	"github.com/jacobsa/comeback/internal/comebackfs"
+	"github.com/jacobsa/comeback/internal/registry"
+	"github.com/jacobsa/comeback/internal/util"
+	"github.com/jacobsa/comeback/internal/wiring"
+)
+
 var cmdMount = &Command{
 	Name: "mount",
 }
-
-var fScore = cmdMount.Flags.String(
-	"score",
-	"",
-	"Score of backup to mount, if not the default.")
 
 func init() {
 	cmdMount.Run = runMount // Break flag-related dependency loop.
 }
 
+func doMount(args []string) (err error) {
+	// Grab dependencies.
+	bucket := getBucket()
+	crypter := getCrypter()
+
+	// Check usage.
+	if len(args) > 1 {
+		err = fmt.Errorf("Usage: %s [score]", os.Args[0])
+		return
+	}
+
+	// Figure out which score to mount.
+	var score blob.Score
+	if len(args) > 0 {
+		score, err = blob.ParseHexScore(args[0])
+		if err != nil {
+			err = fmt.Errorf("ParseHexScore(%q): %v", args[0], err)
+			return
+		}
+	} else {
+		r := getRegistry()
+
+		// List jobs.
+		var jobs []registry.CompletedJob
+		jobs, err = r.ListBackups()
+		if err != nil {
+			err = fmt.Errorf("ListBackups: %v", err)
+			return
+		}
+
+		if len(jobs) == 0 {
+			err = errors.New("No completed jobs found.")
+			return
+		}
+
+		// Find the job with the newest start time.
+		j := jobs[0]
+		for _, candidate := range jobs {
+			if j.StartTime.Before(candidate.StartTime) {
+				j = candidate
+			}
+		}
+
+		score = j.Score
+	}
+
+	// Create the blob store.
+	blobStore, err := wiring.MakeBlobStore(
+		bucket,
+		crypter,
+		util.NewStringSet())
+
+	// Create the file system.
+	_, err = comebackfs.NewFileSystem(score, blobStore)
+	if err != nil {
+		err = fmt.Errorf("NewFileSystem: %v", err)
+		return
+	}
+
+	err = errors.New("TODO")
+	return
+}
+
 func runMount(args []string) {
-	panic("TODO")
+	err := doMount(args)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
