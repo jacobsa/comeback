6d275b8578772991d440f637a956fa063298b78f
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-08-24 15:42:15 +1000

    Fixed up verify/successor_finder.go.

--- internal/verify/successor_finder.go
+++ internal/verify/successor_finder.go
@@ -29,12 +29,12 @@ import (
 // Create a successor finder for the DAG of blobs in the supplied bucket. Nodes
 // are expected to be of type Node.
 //
-// The visitor reads directory blobs, parses them, and returns their children
-// as adjacent nodes. For file nodes, the visitor verifies that their score
-// exists (according to allScores), and verifies that the blob can be loaded if
-// readFiles is true. A record is written to the supplied channel for
-// everything that is verified. This makes this SuccessorFinder a bit weird, in
-// that it has side effects.
+// The successor finder reads directory blobs, parses them, and returns their
+// children as adjacent nodes. For file nodes, the successor finder verifies
+// that their score exists (according to allScores), and verifies that the blob
+// can be loaded if readFiles is true. A record is written to the supplied
+// channel for everything that is verified. This makes this SuccessorFinder a
+// bit weird, in that it has side effects.
 //
 // If work is to be preserved across runs, knownStructure should be filled in
 // with parenthood information from previously-generated records (for both
@@ -50,7 +50,7 @@ func newSuccessorFinder(
 	records chan<- Record,
 	clock timeutil.Clock,
 	bs blob.Store) (sf graph.SuccessorFinder) {
-	typed := &visitor{
+	typed := &successorFinder{
 		readFiles:      readFiles,
 		records:        records,
 		clock:          clock,
@@ -63,7 +63,7 @@ func newSuccessorFinder(
 		typed.knownScores[score] = struct{}{}
 	}
 
-	v = typed
+	sf = typed
 	return
 }
 
@@ -71,7 +71,7 @@ func newSuccessorFinder(
 // Implementation
 ////////////////////////////////////////////////////////////////////////
 
-type visitor struct {
+type successorFinder struct {
 	readFiles      bool
 	records        chan<- Record
 	clock          timeutil.Clock
@@ -80,25 +80,25 @@ type visitor struct {
 	knownStructure map[Node][]Node
 }
 
-func (v *visitor) visitFile(
+func (sf *successorFinder) processFile(
 	ctx context.Context,
 	n Node) (err error) {
 	// If reading files is disabled, there is nothing further to do.
-	if !v.readFiles {
+	if !sf.readFiles {
 		return
 	}
 
 	// Make sure we can load the blob contents. Presumably the blob store
 	// verifies the score (of the ciphertext) on the way through.
-	_, err = v.blobStore.Load(ctx, n.Score)
+	_, err = sf.blobStore.Load(ctx, n.Score)
 	if err != nil {
-		err = fmt.Errorf("Load(%s): %v", n.Score.Hex(), err)
+		err = fmt.Errorf("Load(%s): %sf", n.Score.Hex(), err)
 		return
 	}
 
 	// Certify that we verified the file piece.
 	r := Record{
-		Time: v.clock.Now(),
+		Time: sf.clock.Now(),
 		Node: n,
 	}
 
@@ -107,17 +107,17 @@ func (v *visitor) visitFile(
 		err = ctx.Err()
 		return
 
-	case v.records <- r:
+	case sf.records <- r:
 	}
 
 	return
 }
 
-func (v *visitor) visitDir(
+func (sf *successorFinder) processDir(
 	ctx context.Context,
-	parent Node) (adjacent []string, err error) {
+	parent Node) (successors []graph.Node, err error) {
 	// Load the blob contents.
-	contents, err := v.blobStore.Load(ctx, parent.Score)
+	contents, err := sf.blobStore.Load(ctx, parent.Score)
 	if err != nil {
 		err = fmt.Errorf("Load(%s): %v", parent.Score.Hex(), err)
 		return
@@ -132,7 +132,7 @@ func (v *visitor) visitDir(
 
 	// Build a record containing a child node for each score in each entry.
 	r := Record{
-		Time: v.clock.Now(),
+		Time: sf.clock.Now(),
 		Node: parent,
 	}
 
@@ -178,38 +178,38 @@ func (v *visitor) visitDir(
 		err = ctx.Err()
 		return
 
-	case v.records <- r:
+	case sf.records <- r:
 	}
 
-	// Return child node names.
+	// Copy the directory's children to our return value.
 	for _, child := range r.Children {
-		adjacent = append(adjacent, child.String())
+		successors = append(successors, child)
 	}
 
 	return
 }
 
-func (v *visitor) Visit(
+func (sf *successorFinder) FindDirectSuccessors(
 	ctx context.Context,
-	nodeName string) (adjacent []string, err error) {
-	// Parse the node name.
-	n, err := ParseNode(nodeName)
-	if err != nil {
-		err = fmt.Errorf("ParseNode(%q): %v", nodeName, err)
+	untyped graph.Node) (successors []graph.Node, err error) {
+	// Make sure the node is of the appropriate type.
+	n, ok := untyped.(Node)
+	if !ok {
+		err = fmt.Errorf("Unexpected node type: %T", untyped)
 		return
 	}
 
 	// Make sure the score actually exists.
-	if _, ok := v.knownScores[n.Score]; !ok {
+	if _, ok := sf.knownScores[n.Score]; !ok {
 		err = fmt.Errorf("Unknown score for node: %s", n.String())
 		return
 	}
 
 	// If we have already verified this node, there is nothing further to verify.
-	// Return the appropriate adjacent node names.
-	if children, ok := v.knownStructure[n]; ok {
+	// Return the appropriate successors.
+	if children, ok := sf.knownStructure[n]; ok {
 		for _, child := range children {
-			adjacent = append(adjacent, child.String())
+			successors = append(successors, child)
 		}
 
 		return
@@ -217,10 +217,10 @@ func (v *visitor) Visit(
 
 	// Perform file or directory-specific logic.
 	if n.Dir {
-		adjacent, err = v.visitDir(ctx, n)
+		successors, err = sf.processDir(ctx, n)
 		return
 	} else {
-		err = v.visitFile(ctx, n)
+		err = sf.processFile(ctx, n)
 		return
 	}
 }
