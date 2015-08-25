7a46df484d2028537f319e8c9702da561340a978
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-08-25 11:28:45 +1000

    Added a test stub.

---
+++ internal/save/score_map_test.go
@@ -0,0 +1,63 @@
+// Copyright 2015 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package save
+
+import (
+	"errors"
+	"testing"
+
+	"golang.org/x/net/context"
+
+	"github.com/jacobsa/comeback/internal/state"
+	. "github.com/jacobsa/ogletest"
+	"github.com/jacobsa/timeutil"
+)
+
+func TestScoreMap(t *testing.T) { RunTests(t) }
+
+////////////////////////////////////////////////////////////////////////
+// Boilerplate
+////////////////////////////////////////////////////////////////////////
+
+type ConsultScoreMapTest struct {
+	ctx      context.Context
+	scoreMap state.ScoreMap
+	clock    timeutil.Clock
+
+	node fsNode
+}
+
+var _ SetUpInterface = &ConsultScoreMapTest{}
+
+func init() { RegisterTestSuite(&ConsultScoreMapTest{}) }
+
+func (t *ConsultScoreMapTest) SetUp(ti *TestInfo) {
+	t.ctx = ti.Ctx
+	AssertTrue(false, "TODO")
+}
+
+func (t *ConsultScoreMapTest) call() (err error) {
+	err = errors.New("TODO")
+	return
+}
+
+////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////
+
+func (t *ConsultScoreMapTest) DoesFoo() {
+	AssertTrue(false, "TODO")
+}
