70f1e0f23a61abb8e2d87173738ed3eb3ea5524e
Aaron Jacobs <aaronjjacobs@gmail.com>
2015-08-26 16:14:29 +1000

    Fixed the test package name.

--- internal/dag/visit_test.go
+++ internal/dag/visit_test.go
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package graph_test
+package dag_test
 
 import (
 	cryptorand "crypto/rand"
