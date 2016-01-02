0b30b712d557c5cbc201a88eeaedc6642dc62bc3
Aaron Jacobs <aaronjjacobs@gmail.com>
2016-01-02 20:54:11 +1100

    Revert "Strip out SIGINT handling."
    This is still helpful with the --foreground flag.
    
    This reverts commit 23a8af8cb1de9d5183a230992a0f2bb06ba7d445.

--- mount.go
+++ mount.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"os/user"
 	"strconv"
 
@@ -56,6 +57,28 @@ func init() {
 ////////////////////////////////////////////////////////////////////////
 // Helpers
 ////////////////////////////////////////////////////////////////////////
+
+func registerSIGINTHandler(mountPoint string) {
+	// Register for SIGINT.
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	// Start a goroutine that will unmount when the signal is received.
+	go func() {
+		for {
+			<-signalChan
+			log.Println("Received SIGINT, attempting to unmount...")
+
+			err := fuse.Unmount(mountPoint)
+			if err != nil {
+				log.Printf("Failed to unmount in response to SIGINT: %v", err)
+			} else {
+				log.Printf("Successfully unmounted in response to SIGINT.")
+				return
+			}
+		}
+	}()
+}
 
 // Return the UID and GID of the current process.
 func currentUser() (uid uint32, gid uint32, err error) {
@@ -256,6 +279,9 @@ func runMount(ctx context.Context, args []string) (err error) {
 
 	logger.Println("File system successfully mounted.")
 	daemonize.SignalOutcome(nil)
+
+	// Watch for SIGINT.
+	registerSIGINTHandler(mountPoint)
 
 	// Wait for unmount.
 	err = mfs.Join(ctx)
