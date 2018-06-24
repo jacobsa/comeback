4106bbbb5257b2662473726e66696700e45a09db
Aaron Jacobs <jacobsa@google.com>
2018-06-24 13:23:55 +1000

    more

--- create_blobs/main.go
+++ create_blobs/main.go
@@ -9,6 +9,9 @@ import (
 	"errors"
 	"fmt"
 	"os"
+
+	"github.com/jacobsa/gcloud/gcs"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -19,6 +22,40 @@ func main() {
 }
 
 func run(ctx context.Context) (err error) {
+	// Create the bucket.
+	bucket, err := createBucket(ctx)
+	if err != nil {
+		err = fmt.Errorf("creating bucket: %v", err)
+		return
+	}
+
+	// Create blobs, with parallelism.
+	eg, ctx := errgroup.WithContext(ctx)
+
+	const parallelism = 32
+	for i := 0; i < parallelism; i++ {
+		eg.Go(func() (err error) {
+			err = createBlobs(ctx, bucket)
+			if err != nil {
+				err = fmt.Errorf("creating blobs: %v", err)
+				return
+			}
+
+			return
+		})
+	}
+
+	err = eg.Wait()
+	return
+}
+
+func createBucket(ctx context.Context) (bucket gcs.Bucket, err error) {
+	err = errors.New("TODO")
+	return
+}
+
+// createBlobs repeatedly creates blobs until it fails.
+func createBlobs(ctx context.Context, bucket gcs.Bucket) (err error) {
 	err = errors.New("TODO")
 	return
 }
